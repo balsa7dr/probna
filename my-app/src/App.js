f1fe9c41428a23459a9237a13b5c361245cddb05
balsa7dr <balsadraskovic@gmail.com>
2024-04-12 12:09:18 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,8 @@
+
+import './App.css';
+import Home from './home/Home';
+function App() {
+  const naslov="Balsa";
+  return <Home ime={naslov}></Home>
+}
+export default App;
---
+++ my-app/src/home/Home.js
@@ -0,0 +1,11 @@
+function Home(props){
+    const tekst="Ovo je moja prva aplikacija";
+
+return <div>
+    <h1>{props.ime}</h1>
+    <div>{tekst}</div>
+</div>
+
+
+}
+export default Home;
